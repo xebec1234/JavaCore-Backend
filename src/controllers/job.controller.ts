f96e0e3eea1b22a23788cc200bdabb84e1b613f8
Uriah08 <lorenz8.flores@gmail.com>
2025-09-19 21:00:52 +0800

    User Job

--- src/controllers/job.controller.ts
+++ src/controllers/job.controller.ts
@@ -1,5 +1,6 @@
 import prisma from "../prisma/prisma"
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 export const createJob = async (req: Request, res: Response) => {
     try {
@@ -124,6 +125,38 @@ export const deleteJobs = async (req: Request, res: Response) => {
         })
 
         res.status(200).json({ message: "Jobs Deleted Successfully", success: true });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error", success: false });
+    }
+}
+
+export const getUserJobs = async (req: Request, res: Response) => {
+    try {
+        const token = req.cookies.token;
+
+        if (!token) {
+          return res.status(400).json({ error: "Token Expired", success: false });
+        }
+
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+        if (typeof decoded === "string") {
+        return res
+            .status(400)
+            .json({ error: "Invalid Token Payload", success: false });
+        }
+
+        const jobs = await prisma.job.findMany({
+            orderBy: {
+                no: "asc",
+            },
+            where: {
+                userId: decoded.id
+            },
+        })
+
+        res.status(200).json({jobs, message: "Jobs Fetched Successfully", success: true})
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal Server Error", success: false });

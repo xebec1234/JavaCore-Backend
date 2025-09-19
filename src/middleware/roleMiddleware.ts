b944ecbba7531cd1ee8c4892882af7cb43a64bc6
Uriah08 <lorenz8.flores@gmail.com>
2025-09-19 13:42:13 +0800

    User 2

--- src/middleware/roleMiddleware.ts
+++ src/middleware/roleMiddleware.ts
@@ -3,7 +3,6 @@ const authorizeRoles = (...allowedRoles) => {
         if(!allowedRoles.includes(req.user.role)) {
             return res.status(403).json({ message: "Access Denied" })
         }
-
         next();
     }
 }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import verifyToken from "../middleware/authMiddleware";
 import authorizeRoles from "../middleware/roleMiddleware";
-import { getClients, getMe, register } from "../controllers/user.controller";
+import { changePassword, getClients, getMe, register } from "../controllers/user.controller";
 
 const router = Router();
 
 router.get("/me", verifyToken, authorizeRoles("user","admin"), getMe)
 router.get("/get", verifyToken, authorizeRoles("admin"), getClients)
 router.post("/register", verifyToken, authorizeRoles("admin"), register)
+router.post("/change-password", verifyToken, authorizeRoles("user","admin"), changePassword)
 
 // router.get("/admin", verifyToken, authorizeRoles("admin"), (req,res) => { res.json({ message: "This is Admin"} ) })
 // router.get("/user", verifyToken, authorizeRoles("admin","user"), (req,res) => { res.json({ message: "This is User"} ) })
